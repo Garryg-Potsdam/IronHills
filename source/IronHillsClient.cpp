2a938f80c4b8f13d8507dcb3446e4b1bfe604263
Garry <valepos@gmail.com>
2016-10-17 15:22:59 -0400

    printing data in toString call

--- source/IronHillsClient.cpp
+++ source/IronHillsClient.cpp
@@ -2,13 +2,15 @@
 
 using namespace std;
 
+// this is a file of all the nodes with distance from goals
 const string NODES_WITH_DISTANCES_TO_IRON_HILLS = "distancetable.txt";
 
+// this is a file with all the local connections to other nodes
 const string CONNECTIONS_BETWEEN_NODES = "connectiontable.txt";
 
 int main() {    
     Graph * g;
     g = new Graph(NODES_WITH_DISTANCES_TO_IRON_HILLS);    
-    //cout << g.toString() << endl;
+    cout << g->toString();
     return 0;
 }
