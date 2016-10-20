7e36aa5228f1a2d8f97eb30e74cad3fcd2c3ae76
gregorydhughes <gregorydhughes@gmail.com>
2016-10-20 10:18:35 -0400

    enable user input

--- IronHills/IronHillsClient.cpp
+++ IronHills/IronHillsClient.cpp
@@ -8,12 +8,39 @@ const string NODES_WITH_DISTANCES_TO_IRON_HILLS = "distancetable.txt";
 // this is a file with all the local connections to other nodes
 const string CONNECTIONS_BETWEEN_NODES = "connectiontable.txt";
 
+string GetStringFromQuestion(string question);
+
+int GetIntFromQuestion(string question);
+
 int main() {    
     Graph middleEarth = Graph(NODES_WITH_DISTANCES_TO_IRON_HILLS);
     middleEarth.addEdges(CONNECTIONS_BETWEEN_NODES);    
     
+
+
+
 	cout << middleEarth.toString();
 
 	system("pause");
     return 0;
+}
+
+string GetStringFromQuestion(string question){
+	cout << question << endl;
+	
+	string ans;
+
+	cin >> ans;
+
+	return ans;
+}
+
+int GetIntFromQuestion(string question) {
+	cout << question << endl;
+
+	int ans;
+
+	cin >> ans;
+
+	return ans;
 }
