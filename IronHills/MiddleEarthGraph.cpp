7924207fcd7015a24f29afd7601dce700d333cea
Garry <valepos@gmail.com>
2016-10-25 16:39:31 -0400

    updated comments

--- IronHills/MiddleEarthGraph.cpp
+++ IronHills/MiddleEarthGraph.cpp
@@ -41,102 +41,7 @@ Graph::Graph(string nodesWithDistances) {
 	}
 }
 
-void Graph::aStar(string start, Heuristics h, int complexity) {
-	int currLoc = getNode(start);
-	Path* curr = new Path;
-	curr->fn = 0;
-	curr->location = currLoc;
-	curr->pathSum = 0;
 
-	while (locations[curr->location].locationName.compare(GOAL_LOCATION) != 0) {
-		
-		int edges = locations[curr->location].edgeCount;
-		for (int i = 0; i < edges; i++) {
-			int to = getNode(locations[curr->location].edges[i].to);
-			bool checkDuplicate = false;
-			for (int j = 0; j < curr->path.size(); j++)
-				if (curr->path[j] == to) {
-					checkDuplicate = true;
-					break;
-				}
-			if (checkDuplicate)
-				continue;					
-			int dtih = locations[to].distanceToIronHills;
-			int heur;
-			if (complexity == 1)
-				heur = getComplexOneHeuristic(locations[curr->location].edges[i], h);
-			else if (complexity == 2)
-				heur = getComplexTwoHeuristic(locations[curr->location].edges[i]);
-			else
-				heur = getComplexThreeHeuristic(locations[curr->location].edges[i]);
-
-			int fn = calculateFn(dtih, heur);
-			addPath(to, fn, heur, curr->location, curr);
-		}
-		delete curr;
-		curr = paths.top();
-		paths.pop();
-	}
-	finalPath = curr;
-}
-
-int Graph::getComplexOneHeuristic(Edge e, Heuristics h) {
-	switch (h) {
-	case ROAD_QUALITY:
-		return e.roadQuality;
-		break;
-	case DISTANCE:
-		return e.distance;
-		break;
-	case RISK_LEVEL:
-		return e.riskLevel;
-		break;
-	default:
-		break;
-	}
-}
-
-int Graph::getComplexTwoHeuristic(Edge e) {
-	if (e.riskLevel + e.roadQuality < 0)
-		return e.distance + e.roadQuality;
-	else
-		return e.distance + e.riskLevel;
-}
-
-int Graph::getComplexThreeHeuristic(Edge e) {
-	return e.riskLevel + ((e.roadQuality / 4) * 3) + e.distance;
-}
-
-void Graph::addPath(int loc, int fn, int heur, int parent, const Path* curr) {
-	Path* p = new Path;
-	p->fn = fn;
-	p->location = loc;
-	p->path = curr->path;
-	p->path.push_back(parent);
-	p->pathSum = curr->pathSum + heur;
-	paths.push(p);	
-}
-
-int Graph::calculateFn(int g, int h) {
-	return g + h;
-}
-
-// Parameters : from - the current location
-//                to - the location the edge goes to
-//                 d - the distance of the edge
-//                rq - the road quality of the edge
-//                rl - the risk level of the edge
-// Post-Condition: addes an edge to the from node
-void Graph::addEdge(string from, string to, int d, int rq, int rl) {
-	int temp = getNode(from);
-
-	// I assume you want to do this for all the edges? and not just the edge count
-	locations[temp].edges[locations[temp].edgeCount].to = to;
-	locations[temp].edges[locations[temp].edgeCount].distance = d;
-	locations[temp].edges[locations[temp].edgeCount].roadQuality = (rq * -1);
-	locations[temp].edges[locations[temp].edgeCount].riskLevel = rl;
-	locations[temp].edgeCount++;
-}
 
 // Parameters: nodeConnData - a file of a specific format for adding edges
 void Graph::addEdges(string nodeConnData) {
@@ -167,47 +72,69 @@ void Graph::addEdges(string nodeConnData) {
 	}
 }
 
-// Parameters: s - a string to convert to an int
-// Returns: an int that was converted from a string
-int Graph::stringToInt(string s) {
-	int x;
-	stringstream ss(s);
-	ss >> x;
-	return x;
-}
+// Parameters:     start - the starting location wanted
+//                     h - the heuristic to use in the case of simple run
+//            complexity - the level of complexity to use
+// Post-Condition: solves for the A* path in the graph using the complexity level
+void Graph::aStar(string start, Heuristics h, int complexity) {
+        // the start location
+	int currLoc = getNode(start);
+	
+	// initialize a starting path
+	Path* curr = new Path;
+	curr->fn = 0;
+	curr->location = currLoc;
+	curr->pathSum = 0;
 
-// Returns: int representing the amount of nodes in graph
-int Graph::getSize() {
-	return locCounter - 1;
-}
+	// main loop goes through building paths and using smallest Fn until Iron Hills is found
+	while (locations[curr->location].locationName.compare(GOAL_LOCATION) != 0) {
+		// list of out going edges
+		int edges = locations[curr->location].edgeCount;
+		
+		// check each edge and add a new path object to heap for each		
+		for (int i = 0; i < edges; i++) {
+			// get the other end of the edge
+			int to = getNode(locations[curr->location].edges[i].to);
+			
+			// avoid duplicates
+			bool checkDuplicate = false;
+			
+			// if we have already visited to skip it
+			for (int j = 0; j < curr->path.size(); j++)
+				if (curr->path[j] == to) {
+					checkDuplicate = true;
+					break;
+				}
+			if (checkDuplicate)
+				continue;
+			
+			// distance to iron hills
+			int dtih = locations[to].distanceToIronHills;
+			
+			// heuristic storage
+			int heur;
+			
+			// which heuristic to use
+			if (complexity == 1)
+				heur = getComplexOneHeuristic(locations[curr->location].edges[i], h);
+			else if (complexity == 2)
+				heur = getComplexTwoHeuristic(locations[curr->location].edges[i]);
+			else
+				heur = getComplexThreeHeuristic(locations[curr->location].edges[i]);
 
-// Parameters: node - string name of the node wanted
-// Returns: the node with the name passed in
-int Graph::getNode(string node) {
-	for (int i = 0; i < locCounter; i++)
-		if (locations[i].locationName.compare(node) == 0)
-			return i;
-}
-
-// Parameters: location - the name of the node being added
-//             distance - the distance from that current node
-//                        to the destination
-// Post-Condition: creates a new node adds it to the locations list
-//                 and increments locCounter
-void Graph::addNode(string location, int distance) {
-	locations[locCounter].locationName = location;
-	locations[locCounter].distanceToIronHills = distance;
-	locations[locCounter].edgeCount = 0;
-	locations[locCounter].visited = false;
-	locCounter++;
-}
-
-// Parameters: i - the int to be converted to a string
-// Returns: the string of a passed in int
-string Graph::intToString(int i) {
-	stringstream ss;
-	ss << i;
-	return ss.str();
+			// calculate the f(n)
+			int fn = calculateFn(dtih, heur);
+			
+			// add the new path
+			addPath(to, fn, heur, curr->location, curr);
+		}
+		
+		// delete the used parent path and grab the next smallest fn
+		delete curr;
+		curr = paths.top();
+		paths.pop();
+	}
+	finalPath = curr;
 }
 
 // Post-Condition: Builds a string of all the node data and prints
@@ -222,4 +149,137 @@ string Graph::toString() {
 		printLocs += locations[finalPath->path[i]].locationName + " -> ";
 
 	return printLocs + locations[finalPath->location].locationName + "\n";
+}
+
+// PRIVATE FUNCTIONS
+
+// Parameters: location - the name of the node being added
+//             distance - the distance from that current node
+//                        to the destination
+// Post-Condition: creates a new node adds it to the locations list
+//                 and increments locCounter
+void Graph::addNode(string location, int distance) {
+	locations[locCounter].locationName = location;
+	locations[locCounter].distanceToIronHills = distance;
+	locations[locCounter].edgeCount = 0;
+	locations[locCounter].visited = false;
+	locCounter++;
+}
+
+// Parameters: node - string name of the node wanted
+// Returns: the node with the name passed in
+int Graph::getNode(string node) {
+	for (int i = 0; i < locCounter; i++)
+		if (locations[i].locationName.compare(node) == 0)
+			return i;
+}
+
+// Parameters : from - the current location
+//                to - the location the edge goes to
+//                 d - the distance of the edge
+//                rq - the road quality of the edge
+//                rl - the risk level of the edge
+// Post-Condition: addes an edge to the from node
+void Graph::addEdge(string from, string to, int d, int rq, int rl) {
+	int temp = getNode(from);
+
+	// I assume you want to do this for all the edges? and not just the edge count
+	locations[temp].edges[locations[temp].edgeCount].to = to;
+	locations[temp].edges[locations[temp].edgeCount].distance = d;
+	locations[temp].edges[locations[temp].edgeCount].roadQuality = (rq * -1);
+	locations[temp].edges[locations[temp].edgeCount].riskLevel = rl;
+	locations[temp].edgeCount++;
+}
+
+// Parameters: e - the edge of values
+//             h - the heuristic to return for calulation
+// Returns: a specific edge heuristic value
+int Graph::getComplexOneHeuristic(Edge e, Heuristics h) {
+	switch (h) {
+	case ROAD_QUALITY:
+		return e.roadQuality;
+		break;
+	case DISTANCE:
+		return e.distance;
+		break;
+	case RISK_LEVEL:
+		return e.riskLevel;
+		break;
+	default:
+		break;
+	}
+}
+
+// Parameters: e - the edge of values
+// Returns: a heuristic based of the three edge values
+int Graph::getComplexTwoHeuristic(Edge e) {
+	if (e.riskLevel + e.roadQuality < 0)
+		return e.distance + e.roadQuality;
+	else
+		return e.distance + e.riskLevel;
+}
+
+// Parameters: e - the edge of values
+// Returns: a heuristic based of the three edge values
+int Graph::getComplexThreeHeuristic(Edge e) {
+	return e.riskLevel + ((e.roadQuality / 4) * 3) + e.distance;
+}
+
+
+// Parameters:   loc - the current location of the path;
+//                fn - the fn value of the path
+//              heur - the heuristic value of the current path
+//            parent - the parent for that was recently moved from for this path
+//              curr - the parent path to be used for the new paths
+// Post-Condition: builds a new path and addes it to the heap
+void Graph::addPath(int loc, int fn, int heur, int parent, const Path* curr) {
+	Path* p = new Path;
+	p->fn = fn;
+	p->location = loc;
+	p->path = curr->path;
+	p->path.push_back(parent);
+	p->pathSum = curr->pathSum + heur;
+	paths.push(p);	
+}
+
+// Parameters: g - the path total so far
+//             h - the heuristic to be used
+// Returns: the sum of g + h
+int Graph::calculateFn(int g, int h) {
+	return g + h;
+}
+
+// Parameters: i - the int to be converted to a string
+// Returns: the string of a passed in int
+string Graph::intToString(int i) {
+	stringstream ss;
+	ss << i;
+	return ss.str();
+}
+
+// Parameters: s - a string to convert to an int
+// Returns: an int that was converted from a string
+int Graph::stringToInt(string s) {
+	int x;
+	stringstream ss(s);
+	ss >> x;
+	return x;
+}
+
+// Returns: int representing the amount of nodes in graph
+int Graph::getSize() {
+	return locCounter - 1;
+}
+
+// Parameters: location - the name of the node being added
+//             distance - the distance from that current node
+//                        to the destination
+// Post-Condition: creates a new node adds it to the locations list
+//                 and increments locCounter
+void Graph::addNode(string location, int distance) {
+	locations[locCounter].locationName = location;
+	locations[locCounter].distanceToIronHills = distance;
+	locations[locCounter].edgeCount = 0;
+	locations[locCounter].visited = false;
+	locCounter++;
 }
--- IronHills/MiddleEarthGraph.h
+++ IronHills/MiddleEarthGraph.h
@@ -40,6 +40,7 @@ private:
 		bool visited;
 	};
 
+	// Path struct that holds the values needed to find lowest fn path
 	struct Path {
 		int location;
 		vector<int> path;
@@ -47,27 +48,25 @@ private:
 		int fn;		
 	};
 
+	// Operator overloader for a priority_queue to make it a min heap
 	struct ComparePath : public binary_function<Path*, Path*, bool> {
 		bool operator()(const Path* lhs, const Path* rhs) const {
 			return lhs->fn > rhs->fn;
 		}
 	};
 
+	// the final shortest fn path
 	Path* finalPath;
 
+// 	// keep track of path length
 	int pathCounter;
+	
+	// min heap for grabbing smallest fn paths
 	priority_queue<Path*, vector<Path*>, ComparePath> paths;
 
+	// used for putting in the locations
 	int locCounter;
 	Node locations[25];
-
-	// Parameters: i - the int to be converted to a string
-	// Returns: the string of a passed in int
-	string intToString(int i);
-
-	// Parameters: s - a string to convert to an int
-	// Returns: an int that was converted from a string
-	int stringToInt(string s);
 
 	// Parameters: location - the name of the node being added
 	//             distance - the distance from that current node
@@ -76,8 +75,9 @@ private:
 	//                 and increments locCounter
 	void addNode(string location, int distance);
 
-	// Returns: int representing the amount of nodes in graph
-	int getSize();
+	// Parameters: node - string name of the node wanted
+	// Returns: the node with the name passed in
+	int getNode(string node);
 
 	// Parameters : from - the current location
 	//                to - the location the edge goes to
@@ -86,20 +86,45 @@ private:
 	//                rl - the risk level of the edge
 	// Post-Condition: addes an edge to the from node
 	void addEdge(string from, string to, int d, int rq, int rl);
+	
 
-	// Parameters: node - string name of the node wanted
-	// Returns: the node with the name passed in
-	int getNode(string node);
-
-	int calculateFn(int g, int h);
-
+	// Parameters: e - the edge of values
+	//             h - the heuristic to return for calulation
+	// Returns: a specific edge heuristic value
 	int getComplexOneHeuristic(Edge e, Heuristics h);
+	
+	// Parameters: e - the edge of values
+	// Returns: a heuristic based of the three edge values
 	int getComplexTwoHeuristic(Edge e);
+	
+	// Parameters: e - the edge of values
+	// Returns: a heuristic based of the three edge values
 	int getComplexThreeHeuristic(Edge e);
-
+	
+	// Parameters:   loc - the current location of the path;
+	//                fn - the fn value of the path
+	//              heur - the heuristic value of the current path
+	//            parent - the parent for that was recently moved from for this path
+	//              curr - the parent path to be used for the new paths
+	// Post-Condition: builds a new path and addes it to the heap
 	void addPath(int loc, int fn, int heur, int parent, const Path* curr);
 
+	// Parameters: g - the path total so far
+	//             h - the heuristic to be used
+	// Returns: the sum of g + h
+	int calculateFn(int g, int h);
+	
+		// Parameters: i - the int to be converted to a string
+	// Returns: the string of a passed in int
+	string intToString(int i);
 
+	// Parameters: s - a string to convert to an int
+	// Returns: an int that was converted from a string
+	int stringToInt(string s);
+
+	// Returns: int representing the amount of nodes in graph
+	int getSize();
+	
 public:
 	// Graph Constuctor
 	// Parameters: nodesWithDistances - take a specificly formatted file's name and 
@@ -109,7 +134,10 @@ public:
 	// Parameters: nodeConnData - a file of a specific format for adding edges
 	void addEdges(string nodeConnData);
 
-
+	// Parameters:     start - the starting location wanted
+	//                     h - the heuristic to use in the case of simple run
+	//            complexity - the level of complexity to use
+	// Post-Condition: solves for the A* path in the graph using the complexity level
 	void aStar(string start, Heuristics h, int simple);
 	
 
